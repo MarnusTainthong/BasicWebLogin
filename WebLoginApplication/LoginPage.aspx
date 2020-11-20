c04f20284c1786124db139c015867c8b536669c1
Marnus Tainthong <marnusfrank@gmail.com>
2020-11-20 18:49:40 +0700

    Add project files.

---
+++ WebLoginApplication/LoginPage.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginPage.aspx.cs" Inherits="WebLoginApplication.LoginPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login page</title>
+</head>
+<body>
+    <div>
+        <form id="form1" runat="server">
+            <h1>Login page</h1>
+            <div style="margin-top: 20px">
+                <asp:Label ID="Label1" runat="server" Text="Username"></asp:Label>
+                <asp:TextBox ID="TbUserId" runat="server" ></asp:TextBox>
+            </div>
+            <div style="margin-top: 10px">
+                <asp:Label ID="Label2" runat="server" Text="Password"></asp:Label>
+                <asp:TextBox ID="TbUserPass" runat="server" TextMode="Password"></asp:TextBox>
+            </div>
+            <div style="margin-top: 10px">
+                <asp:Button ID="btnLogin" runat="server" Text="Login" style="width: 100px; height: 30px; margin-top: 10px" OnClick="btnLogin_Click"/>
+            </div>     
+    </form>
+    </div>   
+</body>
+</html>
---
+++ WebLoginApplication/LoginPage.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebLoginApplication
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (readSession())
+            {
+                Response.Redirect("MainPage");
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userId = TbUserId.Text;
+            string userPass = TbUserPass.Text;
+
+            if(userId == "11" && userPass == "22")
+            {
+                writeSession(userId);
+                Response.Redirect("MainPage.aspx");
+            }
+            else
+            {
+                Response.Write("Error!");
+            }
+        }
+
+        private void writeSession(string userId)
+        {
+            Session["userId"] = userId;
+        }
+
+        private bool readSession()
+        {
+            if(Session["userId"] == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
